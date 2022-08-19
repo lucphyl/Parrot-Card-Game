b74f5e2f5704c6de3e9a0299e259ed3d75b230d0
lucphyl <lucphyl@gmail.com>
2022-08-19 14:38:38 -0300

    geração e comparativo de cartas

--- script.js
+++ script.js
@@ -28,10 +28,101 @@ function jogoIlegal() {
   return false;
 }
 
+function aleatorio() {
+  return Math.random() - 0.5;
+}
 
+function desvirarCarta(carta) {
+  if (idTimer === undefined) {
+    idTimer = setInterval(timer, 1000);
+  }
+
+  if (carta.classList.contains("virada")) {
+    return;
+  }
+
+  if (primeiraCarta !== undefined && segundaCarta !== undefined) {
+    return;
+  }
+  carta.classList.add("virada");
+  jogadas++;
+  if (primeiraCarta === undefined) {
+    primeiraCarta = carta;
+  } else {
+    if (segundaCarta === undefined) {
+      segundaCarta = carta;
+
+      if (primeiraCarta.innerHTML === segundaCarta.innerHTML) {
+        //CARTAS IGUAIS
+        resetarCartas();
+        acertos += 2;
+        verificarFim();
+      } else {
+        //CARTAS DIFERENTES
+        setTimeout(virarCartas, 1000);
+      }
+    }
+  }
+}
+
+
+function resetarCartas() {
+  primeiraCarta = undefined;
+  segundaCarta = undefined;
+}
+
+function virarCartas() {
+  primeiraCarta.classList.remove("virada");
+  segundaCarta.classList.remove("virada");
+  resetarCartas();
+}
+
+function renderizarBaralho() {
+  let tabuleiro = document.querySelector(".boardgame");
+
+  for (let i = 0; i < baralho.length; i++) {
+    let cartaTemplate = `
+        <li class="carta virada" onClick="desvirarCarta(this)">
+            <div class='front-face face'>
+                <img src='imagens/front.png'>
+            </div>
+            <div class='back-face face'>
+                <img src='imagens/${baralho[i]}.gif'>
+            </div>
+        </li>
+    `;
+    tabuleiro.innerHTML += cartaTemplate;
+  }
+
+  setTimeout(desvirarTodasCartas, 1000);
+}
+
+function desvirarTodasCartas() {
+  const cartas = document.querySelectorAll(".virada");
+
+  for (let i = 0; i < cartas.length; i++) {
+    cartas[i].classList.remove("virada");
+  }
+}
+
+function comecarJogo() {
+
+  for (let i = 0; i < qtdCartas / 2; i++) {
+    let carta = tipos[i];
+    baralho.push(carta);
+    baralho.push(carta);
+  }
+  baralho.sort(random);
+  renderizarBaralho();
+}
+
+function timer() {
+  document.querySelector(".timer").classList.add("show-timer");
+  tempo++;
+  document.querySelector(".timer").innerHTML = tempo;
+}
 
 function numCartas() {
-  //qtdCartas = Number(prompt("Com quantas cartas você quer jogar?"));
   qtdCartas = 0;
   while (jogoIlegal()) {
     qtdCartas = Number(prompt("Com quantas cartas você quer jogar?"));
